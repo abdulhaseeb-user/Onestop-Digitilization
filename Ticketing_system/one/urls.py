1b9aacd1e23fc7660840e7bf7f27ea1d42804419
Abdul Haseeb Dharwarwala <102406410+abdulhaseeb-user@users.noreply.github.com>
2023-11-29 20:59:30 +0500

    Delete Ticketing_system directory

--- Ticketing_system/one/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.advising_view, name='student_advising'),
-    path('quick_help/', views.quick_help, name='quick_help_page'),
-    path('ticket_details/<int:ticket_id>/', views.ticket_details, name='ticket_details'),
-]
